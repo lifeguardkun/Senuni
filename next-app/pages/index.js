8b47de376e1a45c4aa59aceb37a6f5e77f26e848
Jin Fujishiro <lifeguard.utdevelopment@gmail.com>
2025-10-08 15:13:49 +0900

    fetch message

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,115 +1,160 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useState, useRef} from 'react';
+import Script from 'next/script'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+function Home (){
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  // GmailAPI, OAuth instance
+  const OAuthClientRef = useRef(null);
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+  const [IncampusMessages, setIncampusMessages] = useState([]);
+  const [ClassroomMessages, setClassroomMessages] = useState([]);
+
+  // --------------- GmailAPI -------------- //
+  // activate GmailAPI
+  const GMAIL_API_KEY = process.env.NEXT_PUBLIC_GMAIL_API_KEY;
+  const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'; 
+
+  //initialize
+  const LoadGapi = () => {
+    gapi.load('client', initializeGapiClient);
+  }
+
+  // LoadGapi's callback
+  const initializeGapiClient = () => {
+    gapi.client.init({
+      apiKey: GMAIL_API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+  }
+
+
+  //--------------- OAuth --------------- //
+  // activate OAuth
+  const OAUTH_CLIENT_ID = process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID;
+  const SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
+
+  // initialize
+  const LoadGsi = () => {
+    OAuthClientRef.current = google.accounts.oauth2.initTokenClient({
+      client_id: OAUTH_CLIENT_ID,
+      scope: SCOPES,
+      // 認証完了時のコールバック
+      callback: () => {
+        console.log('認証完了')
+        fetchMessages();
+      }
+    });
+  }
+
+  // call OAuth after clicked button
+  const hadleOAuthClick = () => {
+    if (OAuthClientRef.current){
+      OAuthClientRef.current.requestAccessToken({ prompt: 'consent' });
+    }
+  }
+
+
+  // fetch Gmail's content after OAuth
+  const fetchMessages= async () => {
+
+    //----- fetch in Campus messages ----//
+    // メールのメタ情報(idなど)を取得
+    const incampus_fetchMetamessages = await gapi.client.gmail.users.messages.list({
+      'userId': 'me',
+      q: 'from:no-reply-incampus@isc.senshu-u.ac.jp',
+      'maxResults': 15
+    });
+    const incampus_metamessages = incampus_fetchMetamessages.result.messages;
+
+    //メタ情報からメッセージを取得
+    const incampus_fetchMessages = incampus_metamessages.map((metamessage) =>
+      gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': metamessage.id
+      })
+    );
+    const incampus_messages = await Promise.all(incampus_fetchMessages);
+
+    //　情報の抽出・整形
+    const incampus_FilteredMessages = incampus_messages.map(message => {
+      return{
+        snippet: message.result.snippet
+      }
+    })
+    
+    setIncampusMessages(incampus_FilteredMessages);
+
+    //----- fetch Google Classroom messages ----//
+    // メールのメタ情報(idなど)を取得
+    const classroom_fetchMetamessages = await gapi.client.gmail.users.messages.list({
+      'userId': 'me',
+      q: 'from:<no-reply@classroom.google.com>',
+      'maxResults': 15
+    });
+    const classroom_metamessages = classroom_fetchMetamessages.result.messages;
+
+    //メタ情報からメッセージを取得
+    const classroom_fetchMessages = classroom_metamessages.map((metamessage) =>
+      gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': metamessage.id
+      })
+    );
+    const classroom_messages = await Promise.all(classroom_fetchMessages);
+
+    //　情報の抽出・整形
+    const classroom_FilteredMessages = classroom_messages.map(message => {
+      return{
+        snippet: message.result.snippet
+      }
+    })
+    
+    setClassroomMessages(classroom_FilteredMessages);
+
+  }
+
+
+  return(
+    <>
+
+      <Script src="https://apis.google.com/js/api.js" onLoad={LoadGapi} />
+      <Script async defer src="https://accounts.google.com/gsi/client" onLoad={LoadGsi}></Script>
+      
+      
+
+      <button onClick = {hadleOAuthClick}>
+        Authentication
+      </button>
+
+      {IncampusMessages.length > 0 ? (
+        <>
+          <h2>in Campus Messages</h2>
+          <ul>
+            {IncampusMessages.map(message => (
+              <li>
+                {message.snippet}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+
+      {ClassroomMessages.length > 0 ? (
+        <>
+          <h2> Google Classroom Messages</h2>
+          <ul>
+            {ClassroomMessages.map(message => (
+              <li>
+                {message.snippet}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+
+    </>
+  )
 }
+
+export default Home;
